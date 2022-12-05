2d79764eebd66ee6219097ba457ee58f9e0a61b0
sibahtu <redietsibahtu@gmail.com>
2022-12-05 12:42:21 -0600

    strange things

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,11 @@
+import React from "react";
+import { useState, useEffect } from "react";
 import "./App.css";
 
 function App() {
-  return <div className="App">Stranger's Things</div>;
+
+
+  return <div className="App">Stranger,s Things</div>;
 }
 
 export default App;
