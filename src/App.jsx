3bb619295ba1f253c36edb23d410b0f64ab8810f
Tyler Gunlock <ty_gunlock@yahoo.com>
2022-12-06 15:13:18 -0500

    submit form showing up but not posting

--- src/App.jsx
+++ src/App.jsx
@@ -2,14 +2,17 @@ import React, { useState, useEffect } from "react";
 import "./App.css";
 
 import Posts from "./Components/Posts";
+import PostForm from "./Components/PostForm";
 
 function App() {
+  const [posts, setPosts] = useState([]);
 
   return (
     <div className="App">
       <div>Under Construction, check back soon...</div>
       <div>
         <Posts />
+        <PostForm />
       </div>
     </div>
   )
