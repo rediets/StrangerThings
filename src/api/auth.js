dd163686ac33469ad8eee12dc27153bebbbf9b34
Tyler Gunlock <ty_gunlock@yahoo.com>
2022-12-07 13:19:23 -0500

    commit message

--- src/api/auth.js
+++ src/api/auth.js
@@ -3,7 +3,11 @@ import { useState, useEffect } from "react";
 const cohort = '2211-ftb-et-web-ft';
 
 export const getStuff = async () => {
-    const response = await fetch(`https://strangers-things.herokuapp.com/api/${cohort}/posts`);
-    const stuff = response.json();
-    return (stuff.data.posts)
+    try {
+        const response = await fetch(`https://strangers-things.herokuapp.com/api/${cohort}/posts`);
+        const stuff = await response.json();
+        return (stuff.data.posts)
+    } catch (error) {
+        console.error(error);
+    }
 }
