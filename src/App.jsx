4a58d0a13881f0357b88b6089370bf6642445bc9
jake <th3hopperr@gmail.com>
2022-12-07 13:14:47 -0500

    Register

--- src/App.jsx
+++ src/App.jsx
@@ -3,14 +3,19 @@ import "./App.css";
 
 import Posts from "./Components/Posts";
 import PostForm from "./Components/PostForm";
+import Register from "./Components/RegisterUser";
 
 function App() {
   const [posts, setPosts] = useState([]);
+  const [token, setToken] = useState(null);
+
+
 
   return (
     <div className="App">
       <div>Under Construction, check back soon...</div>
       <div>
+        <Register setToken = {setToken}/>
         <Posts />
         <PostForm />
       </div>
--- src/api/auth.js
+++ src/api/auth.js
@@ -2,8 +2,28 @@ import { useState, useEffect } from "react";
 
 const cohort = '2211-ftb-et-web-ft';
 
-export const getStuff = async () => {
-    const response = await fetch(`https://strangers-things.herokuapp.com/api/${cohort}/posts`);
-    const stuff = response.json();
-    return (stuff.data.posts)
-}
+export const registerUser = async (username, password) => {
+    try {
+        const response = await fetch(
+            `https://strangers-things.herokuapp.com/api/${cohort}/users/register/`,
+            {
+                method: "POST",
+                headers: {
+                    "Content-Type": "application/json",
+                },
+                body: JSON.stringify({
+                    user: {
+                        username,
+                        password,
+                    },
+                }),
+            }
+        );
+        const {
+            data: { token },
+        } = await response.json();
+        return token;
+    } catch (error) {
+        console.error(error);
+    }
+};
